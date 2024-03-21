8ef9058f38c7cfad16f9e3d8aee5fbc18e5e8abe
Hicham <3xtox.Hicham@gmail.com>
2024-03-21 14:31:54 +0000

    Implement Pug with Prismic and Express - Install dependancies. - Add Prismic contents to HTML. - Adjust Express to display products and pages.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 require("dotenv").config()
 
+const logger = require("morgan")
 const express = require("express")
+const errorHandler = require("errorhandler")
+const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
+
 const app = express()
 const path = require("path")
 const port = 3000
+
+app.use(logger("dev"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride())
+app.use(errorHandler())
 
 const Prismic = require("@prismicio/client")
 const PrismicDOM = require("prismic-dom")
@@ -17,7 +28,22 @@ const initApi = req => {
 
 }
 
-const handleLinkResolver = () => {
+const handleLinkResolver = (doc) => {
+  if(doc.type === "product")
+  {
+    return `/detail/${doc.uid}`
+  }
+
+  if(doc.type === "collections")
+  {
+    return "/collections"
+  }
+
+  if(doc.type === "about")
+  {
+    return "/about"
+  }
+
   return '/'
 }
 
@@ -29,39 +55,89 @@ app.use((req, res, next) => {
 
   res.locals.PrismicDOM = PrismicDOM
 
+  res.locals.Number = (index) => {
+    return index == 0 ? "One" : index == 1 ? "Two" : index == 2 ? "Three" : index == 3 ? "Four" : ""
+  }
+
+  res.locals.Link =  handleLinkResolver
+
   next()
 })
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 
-app.get('/', (req, res) => {
-  res.render("pages/home")
-})
+const handleRequest = async(api) => {
+  const metadata = await api.getSingle("metadata")
+  const preloader = await api.getSingle("preloader")
+  const navigation = await api.getSingle("navigation")
 
-app.get("/about", (req, res) => {
-  initApi(req).then(api => {
-    api.query(Prismic.Predicates.any("document.type", ["metadata", "about"])).then(response => {
-      const { results } = response
-      const [ metadata, about ] = results
+  return {
+    metadata,
+    preloader,
+    navigation
+  }
+}
 
-      // console.log(metadata, about)
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const home = await api.getSingle("home")
+  const defaults = await handleRequest(api)
 
-      res.render("pages/about", {
-        about,
-        metadata
-      })
-    })
+  const { results: collections } = await api.query(Prismic.Predicates.at("document.type", "collection"), {
+    fetchLinks: "product.image"
   })
 
+  res.render("pages/home", {
+    ...defaults,
+    collections,
+    home
+
+  })
 })
 
-app.get("/collection", (req, res) => {
-  res.render("pages/collection")
+app.get("/about", async(req, res) => {
+      const api = await initApi(req)
+      const defaults = await handleRequest(api)
+      const about = await api.getSingle("about")
+
+      res.render("pages/about", {
+        ...defaults,
+        about,
+      })
 })
 
-app.get("detail/:uid", (req, res) => {
-  res.render("pages/detail")
+app.get("/collections", async(req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle("home")
+
+  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
+    fetchLinks: "product.image"
+  })
+
+  collections.forEach(element => {
+    console.log(element.data.products[0].products_product.data)
+  });
+
+  res.render("pages/collections", {
+    ...defaults,
+    home,
+    collections,
+  })
+})
+
+app.get("/detail/:uid", async(req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const product = await api.getByUID("product", req.params.uid, {
+    fetchLinks: "collection.title"
+  })
+
+  res.render("pages/detail", {
+    ...defaults,
+    product,
+  })
 })
 
 app.listen(port, () => {
