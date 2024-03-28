747e9d581a2c632957c130e23b7f89d9a8f5eee7
Hicham <3xtox.Hicham@gmail.com>
2024-03-28 00:28:44 +0000

    ADD scss files to styles folder - Install include-media dependency. - Add fonts .woff and .woff2 formats. - import scss files in index.scss.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride())
 app.use(errorHandler())
+app.use(express.static(path.join(__dirname, "public")))
 
 const Prismic = require("@prismicio/client")
 const PrismicDOM = require("prismic-dom")
